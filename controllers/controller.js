9e7c4363dcdfc52fed659bd5c9fcef3d993f6beb
Ester Termens <estertermens@hotmail.com>
2019-05-20 13:06:42 +0200

    formulario2

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,34 +1,51 @@
 "use strict";
 //import $ from "jquery";
+//tengo que crear la variable car como global, para poder utilizarla en todas las funciones
+var car;
 function submitForm() {
-    resetea();
+    var formulario = document.getElementById('formCar');
     //recojo los values del formulario
     var plateValue = document.querySelector("#plate").value;
     var brandValue = document.querySelector("#brand").value;
     var colorValue = document.querySelector("#color").value;
-    validation(plateValue, brandValue, colorValue);
+    validationCar(plateValue, brandValue, colorValue);
     createCar(plateValue, brandValue, colorValue);
     //aparece un formulario u otro
     var formCar = document.querySelector("#formCar");
     var formWheel = document.querySelector("#formWheel");
-    /*   if(validation(plateValue,brandValue,colorValue)==true){
-          formCar.style.display="none";
-          formWheel.style.display="block";
-      }else{
-          formCar.style.display="block";
-          formWheel.style.display="none";
-      } */
+    if (validationCar(plateValue, brandValue, colorValue) == true) {
+        formCar.style.display = "none";
+        formWheel.style.display = "block";
+    }
+    else {
+        formCar.style.display = "block";
+        formWheel.style.display = "none";
+    }
+    resetea(formulario);
+}
+//Creo funcion que llama al objeto car y monto la estructura de los datos
+function createCar(plate, brand, color) {
+    car = new Car(plate, color, brand);
+    structure("PLATE: ", car.plate);
+    structure("BRAND: ", car.brand);
+    structure("COLOR: ", car.color);
 }
 function submitFormWheel() {
-    var inputBrand = document.querySelectorAll(".brand");
-    var inputDiameter = document.querySelectorAll(".diameter");
+    var formulario = document.getElementById('formWheel');
+    var inputBrand = document.querySelectorAll("input.brandForm");
+    var inputDiameter = document.querySelectorAll("input.diameterForm");
     for (var i = 0; i < inputBrand.length; i++) {
         var brand = inputBrand[i].value;
         var diameter = inputDiameter[i].value;
+        car.addWheel(new Wheel(Number(diameter), brand));
+        var numElemento = i + 1;
+        structure2(("RUEDA " + numElemento + ":"), car.wheels[i].brand);
+        structure2(("DIAMETRO " + numElemento + ":"), car.wheels[i].diameter);
     }
+    resetea(formulario);
 }
 //validamos datos y hacemos aparecer la caja con los datos
-function validation(element1, element2, element3) {
+function validationCar(element1, element2, element3) {
     var boxCar = document.querySelector(".boxCar");
     if (!element1 || !element2 || !element3) {
         alert('Hay que validar todos los campos del formulario');
@@ -39,12 +56,17 @@ function validation(element1, element2, element3) {
         return true;
     }
 }
-//Creo funcion que llama al objeto car y monto la estructura de los datos
-function createCar(plate, brand, color) {
-    var car = new Car(plate, color, brand);
-    structure("PLATE: ", car.plate);
-    structure("BRAND: ", car.brand);
-    structure("COLOR: ", car.color);
+function validationWheel(brand, diameter) {
+    var boxWheel = document.querySelector(".boxWheel");
+    if (!brand || !diameter) {
+        alert('Hay que validar todos los campos del formulario');
+        boxWheel.style.display = 'none';
+        return false;
+    }
+    else {
+        boxWheel.style.display = 'block';
+        return true;
+    }
 }
 //funcion para crear la estructura de los datos
 function structure(title, element) {
@@ -54,9 +76,16 @@ function structure(title, element) {
     parrafo.append(textCar);
     boxInfo.append(parrafo);
 }
-function resetea() {
-    var boxInfo = document.querySelector("#carInfo");
-    boxInfo.innerHTML = " ";
+//funcion para crear la estructura de los datos
+function structure2(title, element) {
+    var boxInfo = document.querySelector("#wheelInfo");
+    var parrafo = document.createElement('p');
+    var textCar = document.createTextNode(title + element);
+    parrafo.append(textCar);
+    boxInfo.append(parrafo);
+}
+function resetea(form) {
+    form.reset();
 }
 /* var form= document.querySelectorAll("form input");
   
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,43 +1,66 @@
 //import $ from "jquery";
 
+//tengo que crear la variable car como global, para poder utilizarla en todas las funciones
+var car:Car; 
+
+
+
+
 function submitForm(){
-   resetea();
+    var formulario= <HTMLFormElement>document.getElementById('formCar');  
     //recojo los values del formulario
 
     var plateValue= (<HTMLInputElement>document.querySelector("#plate")).value;
     var brandValue= (<HTMLInputElement>document.querySelector("#brand")).value;
     var colorValue= (<HTMLInputElement>document.querySelector("#color")).value;
-    validation(plateValue,brandValue,colorValue);
+    validationCar(plateValue,brandValue,colorValue);
+    
     createCar(plateValue,brandValue,colorValue);
 
     //aparece un formulario u otro
     var formCar= <HTMLInputElement>document.querySelector("#formCar");
     var formWheel= <HTMLInputElement>document.querySelector("#formWheel");
 
-  /*   if(validation(plateValue,brandValue,colorValue)==true){
+    if(validationCar(plateValue,brandValue,colorValue)==true){
         formCar.style.display="none";
         formWheel.style.display="block";
     }else{
         formCar.style.display="block";
         formWheel.style.display="none";
-    } */
+    } 
+    resetea(formulario);
        
+}
+//Creo funcion que llama al objeto car y monto la estructura de los datos
+function createCar(plate:string,brand:string,color:string){
+    car=new Car(plate,color,brand);
+    
+    structure("PLATE: ", car.plate);
+    structure("BRAND: ", car.brand);
+    structure("COLOR: ", car.color);
+
 }
 
 function submitFormWheel(){
-    
-    var inputBrand=document.querySelectorAll(".brand");
-    var inputDiameter=document.querySelectorAll(".diameter");
+    var formulario= <HTMLFormElement>document.getElementById('formWheel');
+    var inputBrand=document.querySelectorAll("input.brandForm");
+    var inputDiameter=document.querySelectorAll("input.diameterForm");
     
    for(let i=0; i<inputBrand.length; i++){
+      
         let brand=(<HTMLInputElement>inputBrand[i]).value;
         let diameter=(<HTMLInputElement>inputDiameter[i]).value;
-
-    }  
+       car.addWheel(new Wheel(Number(diameter),brand));
+       var numElemento= i+1;
+       structure2(("RUEDA " + numElemento + ":" ),car.wheels[i].brand);
+       structure2(("DIAMETRO " + numElemento + ":"), car.wheels[i].diameter);
+    } 
+   
+    resetea(formulario);
 }
 
 //validamos datos y hacemos aparecer la caja con los datos
-function validation(element1:string, element2:string, element3:string){
+function validationCar(element1:string, element2:string, element3:string){
     var boxCar= <HTMLInputElement>document.querySelector(".boxCar");
     if(!element1 || !element2 || !element3){
         alert('Hay que validar todos los campos del formulario');
@@ -48,15 +71,20 @@ function validation(element1:string, element2:string, element3:string){
     }
 }
 
-//Creo funcion que llama al objeto car y monto la estructura de los datos
-function createCar(plate:string,brand:string,color:string){
-    let car=new Car(plate,color,brand);
-    
-    structure("PLATE: ", car.plate);
-    structure("BRAND: ", car.brand);
-    structure("COLOR: ", car.color);
-
+function validationWheel(brand:string, diameter:number){
+    var boxWheel= <HTMLInputElement>document.querySelector(".boxWheel");
+    if(!brand || !diameter){
+        alert('Hay que validar todos los campos del formulario');
+        boxWheel.style.display='none';
+        return false;
+    }else{
+        boxWheel.style.display='block';
+        return true;
+    }
 }
+
+
+
 
 
 
@@ -68,12 +96,18 @@ function structure(title:string, element:string){
     parrafo.append(textCar);
     boxInfo.append(parrafo);
 }
+//funcion para crear la estructura de los datos
+function structure2(title:string, element:any){
+    var boxInfo = <HTMLInputElement>document.querySelector("#wheelInfo");
+    var parrafo= document.createElement('p');
+    var textCar= document.createTextNode(title + element);
+    parrafo.append(textCar);
+    boxInfo.append(parrafo);
+}
 
 
-function resetea(){
-    var boxInfo = <HTMLInputElement>document.querySelector("#carInfo");
-    boxInfo.innerHTML= " ";
-
+function resetea(form:any){
+    form.reset();
 }
 
 
